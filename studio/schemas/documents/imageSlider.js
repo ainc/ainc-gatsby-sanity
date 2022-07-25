fb2b2a52c84c0af65525fb0b5e6bfbd1ce96c337
Kyle Raney <raney24fcb@gmail.com>
2022-07-25 15:36:59 -0400

    add image slider

---
+++ studio/schemas/documents/imageSlider.js
@@ -0,0 +1,34 @@
+export default {
+  name: 'imageSlider',
+  type: 'document',
+  title: 'Image Slider',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Slider Title',
+      description: 'In most cases, this will not be displayed'
+    },
+    {
+      name: 'sliderType',
+      type: 'string',
+      title: 'Slider Type',
+      description: 'Default to the Basic Slider, refer to this page for documentation: https://www.npmjs.com/package/hero-slider',
+      options: {
+        list: [
+          {title: 'Basic Slider', value: 'basic-slider'},
+          {title: 'Navbar Slider', value: 'navbar-slider'},
+          {title: 'Vertical Slider', value: 'vertical-slider'}
+        ],
+        layout: 'radio',
+        direction: 'horizontal',
+      }
+    },
+    {
+      name: 'slides',
+      title: 'Slides',
+      type: 'array',
+      of: [{type: 'sliderImage'}]
+    }
+  ]
+}
---
+++ studio/schemas/objects/sliderImage.js
@@ -0,0 +1,32 @@
+export default {
+  name: 'sliderImage',
+  title: 'Slider Image',
+  type: 'object',
+  fields: [
+    {
+      title: 'Header Title',
+      name: 'title',
+      type: 'string',
+    },
+    {
+      title: 'Header Subtitle',
+      name: 'subtitle',
+      type: 'string',
+    },
+    {
+      title: 'Call to Action',
+      name: 'cta',
+      type: 'link',
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'image'
+    }
+  ],
+  preview: {
+    select: {
+      title: 'title'
+    }
+  }
+}
