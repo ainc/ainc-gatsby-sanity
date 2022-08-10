1685d3622ce5c3447420e24353c5531e20d62eba
Kyle Raney <raney24fcb@gmail.com>
2022-08-10 09:16:29 -0400

    change env vars

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID,
+    projectId: 'y716vocf',
     dataset: process.env.GATSBY_SANITY_DATASET || 'production'
   }
 }
