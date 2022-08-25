b40e39bcfa4fcf278ff98294d9afe4949f47d2e1
Kyle Raney <raney24fcb@gmail.com>
2022-08-25 06:52:07 -0400

    add featured events boolean

--- web/src/components/HorizontalCard/HorizontalCard.js
+++ web/src/components/HorizontalCard/HorizontalCard.js
@@ -3,23 +3,30 @@ import { GatsbyImage } from "gatsby-plugin-image";
 import * as styles from "./HorizontalCard.module.scss";
 import styled from "styled-components";
 import Title from "../../components/UI/Title/Title";
+import Subtitle from "../../components/UI/Subtitle/Subtitle";
 import { Container, Col, Row, Image, Card } from "react-bootstrap";
 
 const HorizontalCard = (props) => {
+
+  const { data, error } = props;
+  
+  const dateFormatted = data.date;
+
   return (
-    <Card className="h-100">
+    <Card>
       <Row>
         <Col xs={4}>
-          <Card.Img src="https://via.placeholder.com/150x150" alt={props.title} />
+          <Card.Img src="https://via.placeholder.com/150x150" alt={props.title} className="m-3" />
         </Col>
         <Col xs={8}>
           <Card.Body>
-            <Card.Title>
-              Card title  
-            </Card.Title>
-            <Card.Text>
-              Card Text
-            </Card.Text>
+            <p className="text--brand fw-bold">
+              1:00 pm <br />
+              <small className="fw-normal text--grey">Awesome Inc</small> 
+            </p>
+            <Subtitle className="fw-bold">
+              Event title
+            </Subtitle>
           </Card.Body>
         </Col>
       </Row>
