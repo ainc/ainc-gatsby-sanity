1241e7af69a0640a1a11d46c7276138ca9ca5660
Will Garman <wgarman702@gmail.com>
2022-08-26 00:51:46 +0000

    /fellowship bottom created,  /portfolio created

---
+++ studio/schemas/documents/fellowshipPortfolio.js
@@ -0,0 +1,37 @@
+export default{
+    name: 'fellowshipPortfolio',
+    type: 'document',
+    title: 'Fellowship Portfolio',
+    fields: [
+        {
+            name: 'year',
+            type: 'date',
+            title: 'Year',
+            description: 'Year the fellowship joined',
+            options: {
+                dateFormat: 'YYYY'
+            }
+        },
+        {
+            name: 'companyName',
+            type: 'string',
+            title: 'Company Name'
+        },
+        {
+            name: 'companyURL',
+            type: 'url',
+            title: 'Company Website'
+        },
+        {
+            name: 'fellowshipImage',
+            type: 'image',
+            title: 'Fellowship Image'
+        },
+        {
+            name: 'description',
+            type: 'text',
+            title: 'Company Description'
+        }
+        
+    ]
+}
