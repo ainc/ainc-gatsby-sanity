f6fd5ba25deb6a4f7ca83a748adb2836e89cba38
colinsmith3 <jayson.colin.smith@gmail.com>
2022-10-27 15:56:14 -0400

    Added components

--- web/src/pages/bootcamp/Components/CompanyGrid.js
+++ web/src/pages/bootcamp/Components/CompanyGrid.js
@@ -8,36 +8,36 @@ function CompanyGrid(props) {
             <Row>
                 <Row className="pt-4 pb-3">
                     <Col>
-                        <StaticImage src={props.src1} alt={props.alt1}></StaticImage>
+                        <img src={props.src1} alt={props.alt1}></img>
                     </Col>
                     <Col>
-                        <StaticImage src={props.scr2} alt={props.alt2}></StaticImage>
+                        <img src={props.scr2} alt={props.alt2}></img>
                     </Col>
                     <Col>
-                        <StaticImage src={props.src3} alt={props.alt3}></StaticImage>
+                        <img src={props.src3} alt={props.alt3}></img>
                     </Col>
                     <Col>
-                        <StaticImage src={props.src4} alt={props.alt4}></StaticImage>
+                        <img src={props.src4} alt={props.alt4}></img>
                     </Col>
                     <Col>
-                        <StaticImage src={props.src5} alt={props.alt5}></StaticImage>
+                        <img src={props.src5} alt={props.alt5}></img>
                     </Col>
                 </Row>
                 <Row className="pb-3 pt-3">
                     <Col>
-                        <StaticImage src={props.src6} alt={props.alt6}></StaticImage>
+                        <img src={props.src6} alt={props.alt6}></img>
                     </Col>
                     <Col>
-                        <StaticImage src={props.src7} alt={props.alt7}></StaticImage>
+                        <img src={props.src7} alt={props.alt7}></img>
                     </Col>
                     <Col>
-                        <StaticImage src={props.src8} alt={props.alt8}></StaticImage>
+                        <img src={props.src8} alt={props.alt8}></img>
                     </Col>
                     <Col>
-                        <StaticImage src={props.src9} alt={props.alt9}></StaticImage>
+                        <img src={props.src9} alt={props.alt9}></img>
                     </Col>
                     <Col>
-                        <StaticImage src={props.src10} alt={props.alt10}></StaticImage>
+                        <img src={props.src10} alt={props.alt10}></img>
                     </Col>
                 </Row>
             </Row>
--- web/src/pages/bootcamp/Components/Testimonial.js
+++ web/src/pages/bootcamp/Components/Testimonial.js
@@ -4,49 +4,62 @@ import Subtitle from '../../../components/UI/Subtitle/Subtitle'
 
 import * as styles from './Testimonial.module.scss'
 
+const TestimonialContent = (props) => {
+    return (
+        <div className={styles.testimonialWindowText}>
+            <Subtitle className="text-uppercase fw-semibold" style={{float: "left"}}>{props.author}</Subtitle>
+            <br></br>
+            <p className="mt-4 lh-lg fs-5">{props.content}</p>
+        </div>
+    )
+}
+
 function Testimonial(props) {
+
+    const [data, setData] = React.useState({
+        author: "",
+        content: ""
+    })
+
+    React.useEffect(() => {
+        setData({...data, ['author']: "Melanie Stoeckle", ['content']: "Melanie came to Bootcamp with previous experiene in graphic design. She landed a job as a developer within a week of graduating from the program. Eventually, she earned the freedom through her employer to work remotely, and now travels with world - her office is wherever there's a wifi connection."})
+    }, [])
+
+    const handleTestimonialClick = (e, author, content) => {
+        e.preventDefault()
+        setData({...data, ['author']: author, ['content']: content})
+    }
+
     return (
         <div className={styles.testimonial}>
             <Row>
                 <Col className="col-2">
                     <div className={styles.bootcampGradImages}>
-                        <Row className="my-3">
-                            <div>
-                                <img src={props.src1} data-testimonial="1" alt="Bootcamp Graduate 1"/>
+                        <Row className="my-4">
+                            <div onClick={(e) => handleTestimonialClick(e, "David Vanderhaar", "David worked part-time at Starbucks, and applied for the Bootcamp on a whim (and with a little encouragement from his wife). In addition to working full-time as a software developer, he also teaches classes helping kids learn to code.")}>
+                                <img src={props.src1} alt="Bootcamp Graduate 1"/>
                             </div>
                         </Row>
-                        <Row className="my-3">
-                            <div>
-                                <img src={props.src2} class="active" data-testimonial="2" alt="Bootcamp Graduate 2" />
+                        <Row className="my-4">
+                            <div onClick={(e) => handleTestimonialClick(e, "Melanie Stoeckle", "Melanie came to Bootcamp with previous experiene in graphic design. She landed a job as a developer within a week of graduating from the program. Eventually, she earned the freedom through her employer to work remotely, and now travels with world - her office is wherever there's a wifi connection.")}>
+                                <img src={props.src2} alt="Bootcamp Graduate 2" />
                             </div>
                         </Row>
-                        <Row className="my-3">
-                            <div>
-                                <img src={props.src3} data-testimonial="3" alt="Bootcamp Graduate 3" />
+                        <Row className="my-4">
+                            <div onClick={(e) => handleTestimonialClick(e, "Brett Fraley", 'Brett made the switch from factory worker to software developer. "I worked in lots of different types of factories for years and my body was just getting run down at such a young age. I knew I needed something else. There is no way I could have afforded time or money for a four-year degree."')}>
+                                <img src={props.src3} alt="Bootcamp Graduate 3" />
                             </div>
                         </Row>
                     </div>
                 </Col>
                 <Col className="col-8">
-                    <Row>
+                    <Row className="mt-3 mb-5">
                         <div className={styles.testimonialWindow}>
-                            <Row>
-                                <div className={styles.testimonialWindowText} data-testimonial="1">
-                                    <Subtitle>{props.name1}</Subtitle>
-                                    <p>{props.testimonial1}</p>
-                                </div>
-                            </Row>
-                            <Row>
-                                <div className={styles.testimonialWindowText} data-testimonial="3">
-                                    <Subtitle>{props.name2}</Subtitle>
-                                    <p>{props.testimonial2}</p>
-                                </div>
-                            </Row>
-                            <Row>
-                                <div className={styles.testimonialWindowText} data-testimonial="3">
-                                    <Subtitle>{props.name3}</Subtitle>
-                                    <p>{props.testimonial3}</p>
-                                </div>
+                            <Row className="">
+                                <TestimonialContent 
+                                author={data.author}
+                                content={data.content}
+                                />
                             </Row>
                         </div>
                     </Row>
