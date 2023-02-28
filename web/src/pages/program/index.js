eb30b095577c7fa1c1c0bd855b210ac9b204d8eb
Megan Sizemore <megan.a.sizemore@gmail.com>
2023-02-28 15:58:18 -0500

    add judges/team section for program guide

--- web/src/pages/program/index.js
+++ web/src/pages/program/index.js
@@ -2,82 +2,183 @@ import { GatsbyImage, StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import Title from '../../components/UI/Title/Title'
+import Layout from "../../components/Layout/Layout";
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import Subtitle from '../../components/UI/Subtitle/Subtitle'
 // import { styles } from '../../styles/Variables'
 import * as styles from '../program/program.module.css'
-
-const ProgramPage = ({data}) => {
-    const allProgram = (data.allSanityProgram.nodes || {});
-    const allSanityFiveAcrossSponsors = (data.allSanityFiveAcrossSponsors.nodes || {});
-    const titleSponsorName = (data.allSanityFiveAcrossSponsors.nodes[0].titleSponsorName || {});
-    const titleSponsorLink = (data.allSanityFiveAcrossSponsors.nodes[0].titleSponsorLink || {});
-    const titleSponsorImage = (data.allSanityFiveAcrossSponsors.nodes[0].titleSponsorImage.asset.gatsbyImageData || {});
-
-    const presentingSponsorName = (data.allSanityFiveAcrossSponsors.nodes[0].presentingSponsorName || {});
-    const presentingSponsorLink = (data.allSanityFiveAcrossSponsors.nodes[0].presentingSponsorLink || {});
-    const presentingSponsorImage = (data.allSanityFiveAcrossSponsors.nodes[0].presentingSponsorImage.asset.gatsbyImageData || {});
-
-    const suppourtingSponsors = (data.allSanityFiveAcrossSponsors.nodes[0].suppourtingSponsors || {});
+import BrandButton from '../../components/UI/BrandButton/BrandButton';
 
 
-    return (
-        <Container fluid className={styles.mainHeading}>
-            <Col>
-            <Row>
-                <Title className='text-uppercase text-center text-white mt-5'>Welcome to</Title>
-                </Row>
-                <Row>
-                  <Col sm='10' md='10' lg='6' className='offset-sm-3'>
-                <StaticImage className='my-5 mw-100' src='../../images/5across-banner.png'></StaticImage>
-                </Col>
-            </Row>
-            <Row>
+const ProgramPage = ({ data }) => {
+  const allProgram = (data.allSanityProgram.nodes || {});
+  const allSanityFiveAcrossSponsors = (data.allSanityFiveAcrossSponsors.nodes || {});
+  const titleSponsorName = (data.allSanityFiveAcrossSponsors.nodes[0].titleSponsorName || {});
+  const titleSponsorLink = (data.allSanityFiveAcrossSponsors.nodes[0].titleSponsorLink || {});
+  const titleSponsorImage = (data.allSanityFiveAcrossSponsors.nodes[0].titleSponsorImage.asset.gatsbyImageData || {});
+
+  const presentingSponsorName = (data.allSanityFiveAcrossSponsors.nodes[0].presentingSponsorName || {});
+  const presentingSponsorLink = (data.allSanityFiveAcrossSponsors.nodes[0].presentingSponsorLink || {});
+  const presentingSponsorImage = (data.allSanityFiveAcrossSponsors.nodes[0].presentingSponsorImage.asset.gatsbyImageData || {});
+
+  const suppourtingSponsors = (data.allSanityFiveAcrossSponsors.nodes[0].suppourtingSponsors || {});
+  
+
+  return (
+    <Layout>
+      <Container fluid className={styles.mainHeading}>
+        <Col>
+          <Row>
+            <Title className='text-uppercase text-center text-white mt-5'>Welcome to</Title>
+          </Row>
+          <Row>
+            <Col xs={{ offset: 1, span: 12 }}>
+              <StaticImage className='my-5 mw-100 w-75' src='../../images/5across-banner.png'></StaticImage>
+            </Col>
+          </Row>
+          <Row>
             <ul>
-                                {allProgram.map((node) => (
-                                
-                                   <Subtitle className='text-center'>    {node.date} </Subtitle>
-                                  
-                                ))}                        
-                            </ul>
-            </Row>
-            <Row>
-            <Col xs={3} sm={{ offset: 2, span: 3 }} md={6} lg={6}>
-            <GatsbyImage image={titleSponsorImage}></GatsbyImage>
+              {allProgram.map((node) => (
+
+                <Subtitle className='text-center'>    {node.date} </Subtitle>
+
+              ))}
+            </ul>
+          </Row>
+          <Row>
+            <Col xs={{ offset: 0, span: 8 }}>
+              <GatsbyImage image={titleSponsorImage}></GatsbyImage>
             </Col>
-            <Col xs={3} sm={{ offset: 6, span: 3 }} md={6} lg={6}>
-            <GatsbyImage image={presentingSponsorImage}></GatsbyImage>
+            <Col xs={{ offset: 4, span: 8 }} className=''>
+              <GatsbyImage image={presentingSponsorImage}></GatsbyImage>
             </Col>
-            </Row>
+          </Row>
+        </Col>
+      </Container>
+      <Container className={styles.details}>
+        <Col>
+          <Row>
+            <Subtitle className='text-uppercase mt-3'>Tonight's Details</Subtitle>
+          </Row>
+          <Row>
+            <Col xs={6} className=''>
+              <BrandButton xs={6} className="secondary my-1 w-100">VOTE ON RAADZ</BrandButton>
+              <BrandButton xs={6} className="secondary w-100">JUDGES</BrandButton>
+              <BrandButton xs={6} className="secondary my-1 w-100">PODCASTS</BrandButton>
+              <BrandButton xs={6} className="secondary w-100 text-uppercase mb-2">free ticket for next 5 across</BrandButton>
             </Col>
-        </Container>
-    )
+            <Col xs={6} className=''>
+              <BrandButton xs={6} className="secondary my-1 w-100">TEAMS</BrandButton>
+              <BrandButton xs={6} className="secondary w-100">SPONSORS</BrandButton>
+              <BrandButton xs={6} className="secondary my-1 w-100">BECOME A MENTOR</BrandButton>
+              <BrandButton xs={6} className="secondary w-100 text-uppercase">startup pipeline</BrandButton>
+            </Col>
+          </Row>
+          <Row xs={12}>
+            <Col xs={{ offset: 0, span: 12 }}>
+              <BrandButton className="secondary mb-1 w-100">MORE ABOUT AWESOME INC</BrandButton>
+            </Col>
+          </Row>
+        </Col>
+      </Container>
+      <Container className={styles.tonightsTeams}>
+        <Subtitle className='text-center text-uppercase'>Tonight's Teams</Subtitle>
+        <Col>
+          <Row>
+            <ul>
+              {/* {allProgram.map((judges) => (
+                <GatsbyImage image={judges.image.asset.gatsbyImageData}></GatsbyImage>
+              ))} */}
+            </ul>
+          </Row><hr></hr>
+          <Row>
+            {/* <GatsbyImage image={}></GatsbyImage> */}
+          </Row><hr></hr>
+          <Row>
+
+          </Row><hr></hr>
+          <Row>
+
+          </Row><hr></hr>
+          <Row>
+
+          </Row><hr></hr>
+        </Col>
+      </Container>
+      <Container className={styles.tonightsJudges}>
+        <Subtitle className='text-center text-uppercase text-white my-5'>Tonight's Judges</Subtitle>
+        <Col xs={6}>
+
+        </Col>
+        <Col xs={6}>
+
+        </Col>
+      </Container>
+      <Container className={styles.sponsors}>
+        <Row>
+          <Col xs={{ offset: 2, span: 10 }}>
+            <StaticImage className='my-2 mw-100 w-75' src='../../images/5across-banner.png'></StaticImage>
+          </Col>
+        </Row>
+        <Row>
+          <Subtitle className='text-uppercase text-center'>Thanks it's sponsors</Subtitle>
+        </Row>
+        <Row>
+          <Col xs={7} className="my-1">
+            <a href={titleSponsorLink}>
+              <GatsbyImage image={titleSponsorImage} alt={titleSponsorName} />
+            </a>
+          </Col>
+          <Col xs={5} className="mt-4">
+            <a href={presentingSponsorLink}>
+              <GatsbyImage image={presentingSponsorImage} alt={presentingSponsorName} />
+            </a>
+          </Col>
+        </Row>
+        <Row className={styles.supportSponsors}>
+          <Row>
+            {suppourtingSponsors.map((sponsor) => (
+              <Row xs={{ offset: 3, span: 5 }} className="my-3">
+                <a href={sponsor.link}>
+                  <GatsbyImage image={sponsor.image.asset.gatsbyImageData} alt={sponsor.alt} />
+                </a>
+              </Row>
+
+            ))}
+          </Row>
+        </Row>
+      </Container>
+      <Container className={styles.experienceMore}>
+
+      </Container>
+      <Container className={styles.becomeMentor}>
+
+      </Container>
+    </Layout>
+  )
 };
 
-export const query_program = graphql `
+export const query_program = graphql`
 query query_program {
   allSanityProgram {
     nodes {
       date(formatString: "MMMM D, YYYY")
       teams {
-        image {
-          _key
-          _type
-          _rawAsset
-          _rawHotspot
-          _rawCrop
-        }
         title
+        image {
+          asset {
+            gatsbyImageData(layout: CONSTRAINED)
+          }
+        }
       }
       judges {
         image {
-          _key
-          _type
-          _rawAsset
-          _rawHotspot
-          _rawCrop
+          asset {
+            gatsbyImageData(layout: CONSTRAINED)
+          }
         }
         title
+        alt
       }
     }
   }
