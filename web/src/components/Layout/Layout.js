9daf96a0c86743179cd484685dc29fe5a1d52599
Will Garman <wgarman702@gmail.com>
2023-03-03 00:24:43 -0500

    Created 5A Banner

--- web/src/components/Layout/Layout.js
+++ web/src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useStaticQuery, graphql } from 'gatsby';
-// import Banner from '../Banner/Banner'
+import Banner from '../Banner/Banner'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 
@@ -10,7 +10,7 @@ import "../../styles/layout.css";
 const Layout = ({ pageTitle, children, onHideNav, onShowNav, showNav, siteTitle }) => (
   <div>
     <title>{pageTitle} | This will be dynamic later</title>
-    {/* <Banner /> */}
+    <Banner />
     <Header />
     <div>
       {children}
