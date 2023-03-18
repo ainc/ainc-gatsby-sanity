e7ac26914089695f4f243df5fa9517356d7f1519
Will Garman <wgarman702@gmail.com>
2023-03-18 01:26:55 -0400

    Major updato backend

--- web/src/pages/unsubscribe/index.js
+++ web/src/pages/unsubscribe/index.js
@@ -1,44 +1,95 @@
-import * as React from 'react';
+import React, { useState } from "react";
 import Layout from '../../components/Layout/Layout';
 import { Container, Row, Col } from 'react-bootstrap';
 import Title from '../../components/UI/Title/Title';
 import Subtitle from '../../components/UI/Subtitle/Subtitle';
 import BrandButton from "../../components/UI/BrandButton/BrandButton";
+import * as styles from './unsubscribe.module.scss';
+import { IoIosCheckmarkCircleOutline } from "react-icons/io"
 
-function emailValidate() {
-    var email = document.forms['unsubForm']['email'].value;
+const UnsubscribePage = () => {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const updateEmail = (event) => {
+    setEmail(event.target.value);
+    setMessage("");
+  };
+
+  function emailValidate() {
     document.forms['unsubForm']['email'].value = email.trim();
     var atpos = email.indexOf('@');
     var dotpos = email.lastIndexOf('.');
     if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length) {
-      alert('Please enter a valid email address.');
       return false;
+    }
+    else {
+      setMessage('Valid Email!');
+      console.log(email);
+      return true;
     }
   }
 
-const UnsubscribePage = ({ data }) => {
-    return (
-      <Layout pageTitle="Unsubscribe | Awesome Inc">
-        <Container>
-          <Col className="my-5" md={{ span: 3, offset: 3 }}>
-            <Subtitle className="fw-bold">We already miss you</Subtitle>
-            <div>
-            <form
-                action="https://crm.zoho.com/crm/Unsubscribe?encoding=UTF-8"
-                method="POST"
-                name="unsubForm"
-                onSubmit={() => emailValidate()}>
-                <input type="hidden" name="xnQsjsdp" value="c871ae0722ead1244df94d1b3a4878386566663dc2d3de012c2a68b0fa43efda" />
-                <input type="hidden" name="actionType" value="dW5zdWJzY3JpYmU=" />
-                <input type="hidden" name="returnURL" value="https://www.awesomeinc.org/" />
-                <input className="form-control" placeholder="Email Address" type="text" name="email" maxLength="120" style={{ marginBottom: '1rem' }} />
-                <BrandButton type="submit">Unsubscribe</BrandButton>
-            </form>
-            </div>
-          </Col>
-        </Container>
-      </Layout>
-    );
+  const handleSubmit = (event) => {
+    let emailValidated = emailValidate();
+    event.preventDefault();
+    if (!emailValidated) {
+      setMessage('Please enter a valid email address.');
+    }
+    else {
+    
+    const data = {
+      actionType: "dW5zdWJzY3JpYmU=",
+      returnURL: "https://www.awesomeinc.org/",
+      xnQsjsdp:
+        "c871ae0722ead1244df94d1b3a4878386566663dc2d3de012c2a68b0fa43efda",
+      email: email,
+    };
+    fetch("https://crm.zoho.com/crm/Unsubscribe?encoding=UTF-8", {
+      method: "POST",
+      body: JSON.stringify(data),
+      mode: "no-cors"
+    })
+      .then((response) => {
+        if (response.status != 500) {
+          setMessage("Unsubscribed successfully!");
+          window.location.href = "/";
+        } else {
+          setMessage("Error unsubscribing. Please try again.");
+        }
+      })
+      .catch((error) => {
+        setMessage("Error unsubscribing. Please try again.");
+      });
+    }
   };
+
+  return (
+    <Layout pageTitle="Unsubscribe | Awesome Inc">
+      <Container>
+        <Col className="my-5" md={{ span: 6, offset: 3 }}>
+          <Subtitle className="fw-bold">We already miss you</Subtitle>
+          <div>
+            <form
+              name="unsubForm"
+              onSubmit={handleSubmit}>
+                <input
+                  className="form-control my-3"
+                  placeholder="Email Address"
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(event) => updateEmail(event)}
+                  required
+                />
+                <BrandButton type="submit">Unsubscribe</BrandButton>
+                <p className={styles.message}>{message}</p>
+            </form>
+          </div>
+        </Col>
+      </Container>
+    </Layout>
+  );
+};
   
-  export default UnsubscribePage;
+export default UnsubscribePage;
