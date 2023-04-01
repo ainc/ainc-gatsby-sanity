1871bc72bec7a18a0b66a9b3394d8d70e8379ea6
Will Garman <wgarman702@gmail.com>
2023-04-01 04:06:58 -0400

    Worked on  modals and some small styling

--- web/src/components/Forms/YouthProgramGuide.js/YouthProgramForm.js
+++ web/src/components/Forms/YouthProgramGuide.js/YouthProgramForm.js
@@ -8,35 +8,34 @@ import Card from 'react-bootstrap/Card'
 
 const YouthProgramGuide = ( props ) => {
     return (
-        <Card className={`${styles.formCard} me-5`}>
-<Form>
-    <Container>
-        <Card.Header className={styles.cardHeader}>Download our program guide for more info
-</Card.Header>
-    </Container>
-    <Container className={styles.formContainer}>
-    <Row>
-        <Col>
-    <Form.Group className='pt-3 mb-3' constrolId='formFirstName'>
-        <Form.Control className={styles.formText} type='text' placeholder="First Name"/>
-    </Form.Group>
-    </Col>
-    <Col>
-    <Form.Group className='pt-3 mb-3' constrolId='formLastName'>
-        <Form.Control className={styles.formText} type='text' placeholder="Last Name"/>
-    </Form.Group>
-    </Col>
-    </Row>
-    <Form.Group className='mb-3' constrolId='formPhoneNumber'>
-        <Form.Control className={styles.formText} type='text' placeholder="Phone Number"/>
-    </Form.Group>
-    <Form.Group className='mb-3' constrolId='formInputEmail'>
-        <Form.Control className={styles.formText} type='email' placeholder="Email Address"/>
-    </Form.Group>
-    <BrandButton className='mb-3 text-uppercase'>Download</BrandButton>
-    </Container>
-</Form>
-</Card>
+        <Card className={`${styles.formCard} me-5 background--gray p-0`}>
+            <Form>
+                <Container fluid className="g-0 p-0">
+                    <Card.Header className={styles.cardHeader}>Download our program guide for more info</Card.Header>
+                </Container>
+                <Container className={styles.formContainer}>
+                    <Row>
+                        <Col>
+                            <Form.Group className='pt-3 mb-4' constrolId='formFirstName'>
+                                <Form.Control size="lg"className={styles.formText} type='text' placeholder="First Name" required/>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className='pt-3 mb-4' constrolId='formLastName'>
+                                <Form.Control size="lg"className={styles.formText} type='text' placeholder="Last Name" required/>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Form.Group className='mb-4' constrolId='formPhoneNumber'>
+                        <Form.Control size="lg"className={styles.formText} type='text' placeholder="Phone Number" required/>
+                    </Form.Group>
+                    <Form.Group className='mb-4' constrolId='formInputEmail'>
+                        <Form.Control size="lg" className={styles.formText} type='email' placeholder="Email Address" required/>
+                    </Form.Group>
+                    <BrandButton className='mb-3 text-uppercase'>Download</BrandButton>
+                </Container>
+            </Form>
+        </Card>
     )
 };
 
