89eba2c1ac492cb12cb3aa2632e126c47b9ba6d4
Megan Sizemore <megan.a.sizemore@gmail.com>
2023-04-12 13:03:10 -0400

    sanity doc and images for mentors page

---
+++ studio/schemas/documents/mentors.js
@@ -0,0 +1,28 @@
+export default {
+    name: 'mentor',
+    type: 'document',
+    title: 'Mentors',
+
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Full Name'
+        },
+        {
+            name: 'picture',
+            type: 'image',
+            title: 'Picture'
+        },
+        {
+            name: 'occupation',
+            type: 'string',
+            title: 'Occupation'
+        },
+        {
+            name: 'linkedIn',
+            type: 'url',
+            title: 'LinkedIn URL'
+        }
+    ]
+}
