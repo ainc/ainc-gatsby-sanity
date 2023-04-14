ed0e4f79b6ee6b68852d93da976951eac14bf3bc
Will Garman <wgarman702@gmail.com>
2023-04-13 23:56:57 -0400

    Fixed blog images

--- web/src/components/SocialMediaIcons/SocialMediaIcons.js
+++ web/src/components/SocialMediaIcons/SocialMediaIcons.js
@@ -1,39 +1,24 @@
 import React from "react";
 
-import { AiFillFacebook, AiFillInstagram, AiFillLinkedin, AiFillTwitterSquare, AiFillYoutube } from "react-icons/ai";
+import { AiFillFacebook, AiFillLinkedin, AiFillTwitterSquare } from "react-icons/ai";
 import { Container, Row, Col, Image } from "react-bootstrap";
 
 
-const SocialMediaIcons = () => {
+const SocialMediaIcons = ({text, link}) => {
+    console.log(text, link);
+    const urlText = encodeURIComponent(text)
+    const urlLink = encodeURIComponent(link)
     return (
         <div>
-            <ul>
-                <li>
-                    <a href="https://twitter.com/awesomeinclex">
-                        <AiFillTwitterSquare />
-                    </a>
-                </li>
-                <li>
-                <a href="https://www.facebook.com/awesomeinclex">
-                    <AiFillFacebook />
-                </a>
-                </li>
-                <li>
-                <a href="https://www.linkedin.com/school/awesome-inc/">
-                    <AiFillLinkedin />
-                </a>
-                </li>
-                <li>
-                <a href="https://www.instagram.com/awesomeinclex/">
-                    <AiFillInstagram />
-                </a>
-                </li>
-                <li>
-                <a href="https://www.youtube.com/user/AincTelevision/featured">
-                    <AiFillYoutube />
-                </a>
-                </li>
-            </ul>
+            <a href={`https://twitter.com/intent/tweet?text=${urlText}%0D%0A${urlLink}`} target="_blank" className="d-inline-block ">
+                <AiFillTwitterSquare size={35} color="#1C9BF0" className="rounded-5"/>
+            </a>
+            <a href={`https://www.facebook.com/sharer/sharer.php?u=${urlLink}`} target="_blank" className="d-inline-block">
+                <AiFillFacebook size={35} color="#1877F2" className="rounded-5"/>
+            </a>
+            <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${urlLink}`} target="_blank" className="d-inline-block"> {/* the link for linkedIn wont work unitl the site is hosted*/}
+                <AiFillLinkedin size={35} color="#007BB5" className="rounded-5"/>
+            </a>
         </div>
     );
 };
--- web/src/templates/blog/blog-template.js
+++ web/src/templates/blog/blog-template.js
@@ -1,24 +1,47 @@
-import * as React from 'react'
+import React, {useState} from 'react'
 import BlockContent from '@sanity/block-content-to-react';
 import { graphql, Link } from 'gatsby';
 import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image'
 import BrandButton from '../../components/UI/BrandButton/BrandButton';
+import ModalCustom from '../../components/Modal/ModalCustom';
 import { Col, Container, Row } from 'react-bootstrap';
 import Layout from '../../components/Layout/Layout'
-import { SocialMediaIcons } from '../../components/SocialMediaIcons/SocialMediaIcons';
-
-// import Diagonal from '../../components/Layout/Diagonal/Diagonal'
-// import ButtonAndImage from '../../components/ButtonAndImage/ButtonAndImage'
-// import ImageOutline from '../../components/ImageOutline/ImageOutline'
-// import { StaticImage } from 'gatsby-plugin-image'
+import SocialMediaIcons from '../../components/SocialMediaIcons/SocialMediaIcons';
+import {PortableText} from '@portabletext/react'
+import urlBuilder from '@sanity/image-url'
 import Title from '../../components/UI/Title/Title'
 import Subtitle from '../../components/UI/Subtitle/Subtitle';
-// import Subtitle from "/src/components/UI/Subtitle/Subtitle";
 import '../../styles/main.scss'
 
 const Blog = ({ pageContext }) => {
-    console.log(pageContext.post)
+    const [lgShow, setLgShow] = useState(false);
+    const handleClose = () => setLgShow(false);
+    const handleShow = () => setLgShow(true);
+
     const blogInfo = pageContext.post
+
+    const urlFor = (source) => {
+        return urlBuilder({projectId: 'y716vocf', dataset: 'production'}).image(source)
+    }
+
+    const serializer = {
+        types: {
+            image: props => (
+                // <pre>{JSON.stringify(props)}</pre>
+                <Row>
+                    <Col xs={{span: 10}} lg={{span: 8}}>
+                        <img 
+                            src={urlFor(props.value.asset)
+                                .auto('format')
+                                .url()} 
+                            alt={props.value.alt} 
+                            style={{maxWidth: "100%"}}/>
+                    </Col>
+                </Row>
+            )
+        },
+    }      
+
     return (
         <Layout>
             <Container>
@@ -26,31 +49,39 @@ const Blog = ({ pageContext }) => {
                     <Col sm={{span: 8, offset: 2}}>
                         <Title className="mt-5">{blogInfo.title}</Title>
                         <Container className='my-4'>
-                            {/* <Row> */}
-                                {/* <Col sm={{span: 8}}> */}
                                 <Row className=''>
-                                    <Col sm={{ span: 1}} className="d-flex justify-content-center p-0 rounded-circle border border-1 border-dark">
+                                    <Col xs={{ span: 2}} sm={{span:1}} className="d-flex justify-content-center p-0 rounded-circle border border-1 border-dark">
                                         <GatsbyImage objectFit='cover' image={blogInfo.reference.picture.asset.gatsbyImageData} alt={blogInfo.reference.name} className="rounded-circle border border-3 border-white p-1"/>
                                     </Col>
-                                    <Col sm={{ span: 10}} className="d-flex justify-content-start align-content-center flex-column mt-3 mx-1">
+                                    <Col xs={{ span: 9}} sm={{ span: 10}} className="d-flex justify-content-start align-content-center flex-column mt-0 mt-sm-3 mx-0 mx-sm-1">
                                         <Title className="fs-6 text--brand  mb-0">{blogInfo.reference.name}, {blogInfo.reference.title}</Title>
                                         <h6 className='text--grey'>{blogInfo.date}</h6>
                                     </Col>
                                 </Row>
-                                    {/* <Subtitle>This is blog {blogInfo.title}</Subtitle> */}
-                                {/* </Col> */}
-                            {/* </Row> */}
                         </Container>
-                        <BrandButton className="text-uppercase mb-5">Notify Me</BrandButton>
-                        <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
-                        <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
-                        <a class="a2a_button_facebook"></a>
-                        <a class="a2a_button_twitter"></a>
-                        <a class="a2a_button_email"></a>
-                        </div>
-                        <script async src="https://static.addtoany.com/menu/page.js"></script>
-                        {/* <SocialMediaIcons/> */}
-                        <BlockContent blocks={blogInfo._rawBody} />
+                        <BrandButton className="text-uppercase px-2 py-0 py-sm-1" onClick={handleShow}>Notify Me</BrandButton>
+
+                        <ModalCustom 
+                            lgShow={lgShow} 
+                            hide={handleClose} 
+                            title="Subscribe to our blog" 
+                            content={
+                            //Pass HTML here
+                                <Container>
+                                    <Row>
+                                        <Subtitle>Create form here</Subtitle>
+                                    </Row>
+                                    <Row>
+                                        <BrandButton>Hello</BrandButton>
+                                    </Row>
+                                </Container>
+                            }
+                        />
+
+                        <SocialMediaIcons text={`${blogInfo.title} | Awesome Inc`} link={`https://www.awesomeinc.org/blog/${blogInfo.slug.current}`}/>
+                        <Container className="my-5 px-0">
+                            <PortableText value={blogInfo._rawBody} components={serializer}/>
+                        </Container>
                     </Col>
                 </Row>
             </Container>
