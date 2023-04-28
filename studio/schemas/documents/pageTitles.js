6065869e1cda87b05d76d94ad6024672ceadf03b
Will Garman <wgarman702@gmail.com>
2023-04-28 00:56:38 -0400

    switched order so link is first

--- studio/schemas/documents/pageTitles.js
+++ studio/schemas/documents/pageTitles.js
@@ -5,17 +5,17 @@ export default {
     type: "document",
     fields: [
         {
-            name: "pageTitle",
-            title: "Page Title",
-            type: "string",
-            description: "Every title will be appended with \" | Awesome Inc\"",
-            validation: Rule => Rule.required().min(1)
-        },
-        {
             name: "filePath",
             title: "Page URL",
             type: "string",
             description: "The page URL, e.g. \"/learn/youth\"",
+            validation: Rule => Rule.required().min(1)
+        },
+        {
+            name: "pageTitle",
+            title: "Page Title",
+            type: "string",
+            description: "Every title will be appended with \" | Awesome Inc\"",
             validation: Rule => Rule.required().min(1)
         },
     ],
