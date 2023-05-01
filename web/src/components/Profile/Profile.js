83a2a09df6bfb69e2547fec906327831a3dec71d
Megan Sizemore <megan.a.sizemore@gmail.com>
2023-04-30 21:18:50 -0400

    more styling changes

--- web/src/components/Profile/Profile.js
+++ web/src/components/Profile/Profile.js
@@ -8,7 +8,7 @@ import * as styles from './Profile.module.scss'
 import { btnGroupFullWidth } from "./Profile.module.scss";
 
 
-const Profile = ( { variant, name, image, linkedin, github, website, position, occupation, fact, rule, song, favoritePerson }) => {
+const Profile = ({ variant, name, image, linkedin, github, website, position, occupation, fact, rule, song, favoritePerson }) => {
 
   const [showText, setShowText] = useState(false);
 
@@ -20,92 +20,92 @@ const Profile = ( { variant, name, image, linkedin, github, website, position, o
     setShowText(false);
   };
 
-    
+
   const renderVariantOne = () => (
     <React.Fragment>
-          <Card className='rounded-0'> 
-          <div>
-      <GatsbyImage image={image} className="card-img-top" alt={name} />
-      </div>
-      <Card.Body>
-        { position && <Ribbon position={position}></Ribbon>}
-        <Card.Text className="text-center my-1 fs-6 fw-bold">{name}</Card.Text>
-        { occupation && <p className='text-center text--micro mb-2'>{occupation}</p>}
-        <ButtonGroup size="sm" className={`btn-group ${btnGroupFullWidth}`}>
-          { website && <Button onMouseOver={MouseOver} onMouseOut={MouseOut}href={website} variant="outline-dark">
-            Portfolio
-          </Button>}
-          { linkedin && <Button href={linkedin} variant="outline-dark">
-            LinkedIn
-          </Button>}
-          { github && <Button href={github} variant="outline-dark">
-            GitHub
-          </Button>}
-        </ButtonGroup>
-      </Card.Body>
-    </Card>
+      <Card className='rounded-0'>
+        <div>
+          <GatsbyImage image={image} className="card-img-top" alt={name} />
+        </div>
+        <Card.Body>
+          {position && <Ribbon position={position}></Ribbon>}
+          <Card.Text className="text-center my-1 fs-6 fw-bold">{name}</Card.Text>
+          {occupation && <p className='text-center text--micro mb-2'>{occupation}</p>}
+          <ButtonGroup size="sm" className={`btn-group ${btnGroupFullWidth}`}>
+            {website && <Button onMouseOver={MouseOver} onMouseOut={MouseOut} href={website} variant="outline-dark">
+              Portfolio
+            </Button>}
+            {linkedin && <Button href={linkedin} variant="outline-dark">
+              LinkedIn
+            </Button>}
+            {github && <Button href={github} variant="outline-dark">
+              GitHub
+            </Button>}
+          </ButtonGroup>
+        </Card.Body>
+      </Card>
     </React.Fragment>
   );
 
   const renderVariantTwo = () => (
 
     <React.Fragment>
-      <Card className={`rounded-0 ${styles.profile}`}> 
-      <figure className={styles.figure}>
-      <GatsbyImage
-      image={image} 
-      className={`card-img-top ${styles.profileImage}`} 
-      alt={name}
-       />
-      
-      <Card.Body>
-        { position && <Ribbon position={position}></Ribbon>}
-        <Card.Text className="text-center my-1 fs-6 fw-bold">{name}</Card.Text>
-        { occupation && <p className='text-center text--micro mb-2'>{occupation}</p>}
-       <div className='d-flex justify-content-center'>
-        <ButtonGroup size="sm" className={`btn-group ${btnGroupFullWidth}`}>
-          { fact && <Button className={styles.factButton} variant="outline-dark">
-            Fun Fact
-          </Button>}
-          <figcaption className={styles.figcaption}>
-          <p className={styles.lgxVerticalFact}>{fact}</p>
-      </figcaption>
-          { rule && <Button className={styles.ruleButton} variant="outline-dark">
-            Favorite Rule
-          </Button>}
-          <figcaption className={styles.figcaptionRule}>
-          <p className={styles.lgxVerticalRule}>{rule}</p>
-          </figcaption>
-          { song && <Button className={styles.songButton}  variant="outline-dark">
-            Favorite Song
-          </Button>}
-          <figcaption className={styles.figcaptionSong}>
-          <p className={styles.lgxVerticalSong}>{song}</p>
-          </figcaption>
-          { favoritePerson && <Button className={` rounded-end ${styles.personButton}`} variant="outline-dark">
-            Favorite Person</Button>}
-            <figcaption className={styles.figcaptionPerson}>
-          <p className={styles.lgxVerticalPerson}>{favoritePerson}</p>
-          </figcaption>
-        </ButtonGroup>
-        </div>
-        <div className={styles.test}></div>
-       
-        
-      </Card.Body>
-      </figure>
-    </Card>
+      <Card className={`rounded-0 ${styles.profile}`}>
+        <figure className={styles.figure}>
+          <GatsbyImage
+            image={image}
+            className={`card-img-top ${styles.profileImage}`}
+            alt={name}
+          />
+
+          <Card.Body>
+            {position && <Ribbon position={position}></Ribbon>}
+            <Card.Text className="text-center my-1 fs-6 fw-bold">{name}</Card.Text>
+            {occupation && <p className='text-center text--micro mb-2'>{occupation}</p>}
+            <div className='d-flex justify-content-center'>
+              <ButtonGroup size="sm" className={`btn-group ${btnGroupFullWidth}`}>
+                {fact && <Button className={styles.factButton} variant="outline-dark">
+                  Fun Fact
+                </Button>}
+                <figcaption className={`row ${styles.figcaption}`}>
+                  <p className={styles.lgxVerticalFact}>{fact}</p>
+                </figcaption>
+                {rule && <Button className={styles.ruleButton} variant="outline-dark">
+                  Favorite Rule
+                </Button>}
+                <figcaption className={styles.figcaptionRule}>
+                  <p className={styles.lgxVerticalRule}>{rule}</p>
+                </figcaption>
+                {song && <Button className={styles.songButton} variant="outline-dark">
+                  Favorite Song
+                </Button>}
+                <figcaption className={styles.figcaptionSong}>
+                  <p className={styles.lgxVerticalSong}>{song}</p>
+                </figcaption>
+                {favoritePerson && <Button className={` rounded-end ${styles.personButton}`} variant="outline-dark">
+                  Favorite Person</Button>}
+                <figcaption className={styles.figcaptionPerson}>
+                  <p className={styles.lgxVerticalPerson}>{favoritePerson}</p>
+                </figcaption>
+              </ButtonGroup>
+            </div>
+            <div className={styles.test}></div>
+
+
+          </Card.Body>
+        </figure>
+      </Card>
     </React.Fragment>
   );
 
   const renderContent = () => {
     switch (variant) {
       case "one": // profile card regular
-      return renderVariantOne();
+        return renderVariantOne();
       case "two": // team alpha card
-      return renderVariantTwo();
+        return renderVariantTwo();
       default: //(default) profile card regular
-      return renderVariantOne();
+        return renderVariantOne();
     }
   };
 
--- web/src/pages/team-alpha/index.js
+++ web/src/pages/team-alpha/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 import Layout from '../../components/Layout/Layout'
 import Title from '../../components/UI/Title/Title'
-import {graphql} from 'gatsby'
+import { graphql } from 'gatsby'
 import Profile from '../../components/Profile/Profile'
 import BrandButton from '../../components/UI/BrandButton/BrandButton';
 
@@ -25,49 +25,49 @@ query TeamAlphaPageQuery {
     }
   }`;
 
-  const TeamAlphaPage = ({ data }) => {
-    const allTeamAlpha = (data.allSanityTeamAlpha.nodes || {})
+const TeamAlphaPage = ({ data }) => {
+  const allTeamAlpha = (data.allSanityTeamAlpha.nodes || {})
 
-    return (
-        <Layout>
-            <main>
-                <Container>
-                <Row>
-                    <Title className='text-center mt-3 mb-5 text-uppercase'>Team Alpha</Title>
-                    <Col>
-                    <a className='d-flex justify-content-center' href='https://www.awesomeinc.org/internships#apply'>
-                    <BrandButton className='col-3 mb-5'>Apply Now</BrandButton></a>
-                    </Col>
-                </Row>
-                </Container>
-                <Container className=''>
-                <div className='row justify-content-center'>
-                        {allTeamAlpha.map((node) => (
-                            
-                        <div className='col-sm-10 col-md-5 col-lg-4 col-xl-3 mb-3' key={node.id}>
-                          {/* <Col xs={12} sm={10} md={5} lg={5} xl={5} className='row justify-content-center'> */}
-                      
-                            <Profile
-                                variant="two"
-                                name={node.name}
-                                occupation={node.role}
-                                fact={node.randomFact}
-                                rule={node.favoriteRule}
-                                song={node.favoriteSong}
-                                favoritePerson={node.favoritePerson}
-                                image={node.picture.asset.gatsbyImageData}
-                             ></Profile>
-                            
-                             {/* </Col> */}
-                             </div>
-                             
+  return (
+    <Layout>
+      <main>
+        <Container>
+          <Row>
+            <Title className='text-center mt-3 mb-5 text-uppercase'>Team Alpha</Title>
+            <Col>
+              <a className='d-flex justify-content-center' href='https://www.awesomeinc.org/internships#apply'>
+                <BrandButton className='col-3 mb-5'>Apply Now</BrandButton></a>
+            </Col>
+          </Row>
+        </Container>
+        <Container className=''>
+          <div className='row justify-content-center'>
+            {allTeamAlpha.map((node) => (
 
-                        ))}
-                        </div>
-                </Container>
-            </main>
-        </Layout>
-    )
-  }
+              <div className='col-sm-10 col-md-6 col-lg-4 col-xl-3 mb-3' key={node.id}>
+                {/* <Col xs={12} sm={10} md={5} lg={5} xl={5} className='row justify-content-center'> */}
+
+                <Profile
+                  variant="two"
+                  name={node.name}
+                  occupation={node.role}
+                  fact={node.randomFact}
+                  rule={node.favoriteRule}
+                  song={node.favoriteSong}
+                  favoritePerson={node.favoritePerson}
+                  image={node.picture.asset.gatsbyImageData}
+                ></Profile>
+
+                {/* </Col> */}
+              </div>
+
+
+            ))}
+          </div>
+        </Container>
+      </main>
+    </Layout>
+  )
+}
 
 export default TeamAlphaPage;
