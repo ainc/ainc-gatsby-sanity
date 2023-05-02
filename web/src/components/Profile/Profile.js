15d3bf2ea9185eb8327661a4bbb2a0f37dbc40c4
Megan Sizemore <megan.a.sizemore@gmail.com>
2023-05-02 15:02:26 -0400

    button styling changes

--- web/src/components/Profile/Profile.js
+++ web/src/components/Profile/Profile.js
@@ -20,7 +20,7 @@ const Profile = ({ variant, name, image, linkedin, github, website, position, oc
     setShowText(false);
   };
 
-  {/* /mentors cards */}
+  {/* /mentors cards */ }
   const renderVariantOne = () => (
     <React.Fragment>
       <Card className='rounded-0'>
@@ -47,53 +47,71 @@ const Profile = ({ variant, name, image, linkedin, github, website, position, oc
     </React.Fragment>
   );
 
-  {/* Team alpha cards */}
+  {/* Team alpha cards */ }
   const renderVariantTwo = () => (
 
     <React.Fragment>
       <Card className={`rounded-0 mb-3 ${styles.profile}`}>
         <figure className={styles.figure}>
+
           <GatsbyImage
             image={image}
             className={`card-img-top ${styles.profileImage}`}
             alt={name}
           />
+          
+          <div className='buttonGroup'>
+            <Card.Body>
+              {position && <Ribbon position={position}></Ribbon>}
+              <Card.Text className="text-center my-1 fs-6 fw-bold">{name}</Card.Text>
+              {occupation && <p className='text-center text--micro mb-2'>{occupation}</p>}
+              <div className='d-flex justify-content-center'>
+                <ButtonGroup size="sm" className={`btn-group row row-cols-2 ${btnGroupFullWidth}`}>
+                  {fact && <Button className={`rounded-start ${styles.factButton}`} variant="outline-dark">
+                    Random Fact
+                  </Button>}
+                  <figcaption className={` ${styles.figcaption}`}>
+                    <p className={styles.lgxVerticalFact}><p className='fw-bold ms-0 '>Random Fact: </p>{fact}</p>
+                  </figcaption>
+                  {rule && <Button className={` rounded-end ${styles.ruleButton}`} variant="outline-dark">
+                    Favorite Rule
+                  </Button>}
+                  <figcaption className={styles.figcaptionRule}>
+                    <p className={styles.lgxVerticalRule}><p className='fw-bold ms-0 '>Favorite Rule: </p>{rule}</p>
+                  </figcaption>
+                  {song && <Button className={` rounded-start border-top-0 ${styles.songButton}`} variant="outline-dark">
+                    Favorite Song
+                  </Button>}
+                  <figcaption className={styles.figcaptionSong}>
+                    <p className={styles.lgxVerticalSong}><p className='fw-bold ms-0 '>Favorite Song: </p>{song}</p>
+                  </figcaption>
 
-          <Card.Body>
-            {position && <Ribbon position={position}></Ribbon>}
-            <Card.Text className="text-center my-1 fs-6 fw-bold">{name}</Card.Text>
-            {occupation && <p className='text-center text--micro mb-2'>{occupation}</p>}
-            <div className='d-flex justify-content-center'>
-              <ButtonGroup size="sm" className={`btn-group ${btnGroupFullWidth}`}>
-                {fact && <Button className={styles.factButton} variant="outline-dark">
-                  Fun Fact
-                </Button>}
-                <figcaption className={`row ${styles.figcaption}`}>
-                  <p className={styles.lgxVerticalFact}><p className='fw-bold ms-0 '>Fun Fact: </p>{fact}</p>
-                </figcaption>
-                {rule && <Button className={styles.ruleButton} variant="outline-dark">
-                  Favorite Rule
-                </Button>}
-                <figcaption className={styles.figcaptionRule}>
-                  <p className={styles.lgxVerticalRule}><p className='fw-bold ms-0 '>Favorite Rule: </p>{rule}</p>
-                </figcaption>
-                {song && <Button className={styles.songButton} variant="outline-dark">
-                  Favorite Song
-                </Button>}
-                <figcaption className={styles.figcaptionSong}>
-                  <p className={styles.lgxVerticalSong}><p className='fw-bold ms-0 '>Favorite Song: </p>{song}</p>
-                </figcaption>
-                {favoritePerson && <Button className={` rounded-end ${styles.personButton}`} variant="outline-dark">
-                  Favorite Person</Button>}
-                <figcaption className={styles.figcaptionPerson}>
-                  <p className={`${styles.lgxVerticalPerson}`}><p className='fw-bold ms-0 '>Favorite Person: </p>{favoritePerson}</p>
-                </figcaption>
-              </ButtonGroup>
+                  {favoritePerson && <Button className={` rounded-end border-top-0 ${styles.personButton}`} variant="outline-dark">
+                    Favorite Person</Button>}
+                  <figcaption className={styles.figcaptionPerson}>
+                    <p className={`${styles.lgxVerticalPerson}`}><p className='fw-bold ms-0 '>Favorite Person: </p>{favoritePerson}</p>
+                  </figcaption>
+                </ButtonGroup>
+
+
+              </div>
+              {/* <div className={styles.test}></div>
+
+            <div className="imageBox">
+              <image/>
+              <caption>{fact}</caption>
             </div>
-            <div className={styles.test}></div>
+
+            <div className="buttongroup">
+              <button>fact</button>
+              <button>2</button>
+              <button>3</button>
+              <button>4</button>
+            </div> */}
 
 
-          </Card.Body>
+            </Card.Body>
+          </div>
         </figure>
       </Card>
     </React.Fragment>
