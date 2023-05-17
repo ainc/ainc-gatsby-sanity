0283ff7a1e85e5dbe8b2a1d739560a855e1882ae
Megan Sizemore <megan.a.sizemore@gmail.com>
2023-05-17 14:49:09 -0400

    BackgroundCard responsiveness fixes

--- web/src/components/BackgroundCard/BackgroundCard.js
+++ web/src/components/BackgroundCard/BackgroundCard.js
@@ -2,17 +2,17 @@ import React from "react";
 import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
 import * as styles from "./BackgroundCard.module.scss";
 import styled from "styled-components";
-import { Card } from 'react-bootstrap'
+import { Card, Row, Col, Container } from 'react-bootstrap'
 
 
 const BackgroundCard = (props) => {
   return (
     <a href={props.link}>
-      <Card className={`${styles.cardBackgroundFilter} h-100`} style={{ backgroundImage: `url(${props.backgroundImage})` }}>
-        <Card.Body className="card-body--background-card">
-          <Card.Img variant="top" src={props.imgSrc} className="d-block mx-auto mt-3" style={{ width: `50%` }}/>
-          <Card.Title className="text-uppercase text-white text-center fw-bold fs-6 mt-3 ">{props.title}</Card.Title>
-          <Card.Text className="text-white text-center mb-5 fs-6">{props.text}</Card.Text>
+      <Card className={`${styles.cardBackgroundFilter} col-xs-12`} style={{ backgroundImage: `url(${props.backgroundImage})` }}>
+        <Card.Body className="card-body--background-card p-1">
+          <Card.Img variant="top" src={props.imgSrc} className="d-block mx-auto mt-3 w-50" />
+          <Card.Title className={`text-uppercase text-white text-center fw-bold mt-3 ${styles.cardTitle}`}>{props.title}</Card.Title>
+          <Card.Text className={`text-white text-center mb-5 ${styles.cardText}`}>{props.text}</Card.Text>
         </Card.Body>
       </Card>
     </a>
