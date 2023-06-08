ab1b8aad1af0c67e100ad2a54780b2760e215d32
metraidep <thtranle.2304@gmail.com>
2023-06-08 14:51:37 -0400

    Update last change to course table

--- web/src/components/CourseTable/CourseTable.js
+++ web/src/components/CourseTable/CourseTable.js
@@ -11,46 +11,6 @@ import BrandButton from "../UI/BrandButton/BrandButton";
 import "./coursetable.module.scss"
 
 
-// const CourseTable = ({ tableColumns, tableInfo }) => {
-//     return (
-//         <Table className="">
-//             <tr>
-//                 {tableColumns.map((column) => (
-//                     <th className={styles.titleCell}>
-//                         <h5 className={styles.titleText}>{column}</h5>
-//                     </th>
-//                 ))}
-//             </tr>
-//             {tableInfo.map((course) => (
-//                 <tr>
-//                     <td className={styles.cell}>
-//                     <a href={course.node.courseLink}>
-//                         {course.node.picture !== null ? (
-//                             <GatsbyImage alt={course.node.courseTitle} image={course.node.picture.asset.gatsbyImageData} layout="contrained" width={100}/>
-//                         ) : (
-//                             <Subtitle className="text--brand link--bright-red">{course.node.courseTitle}</Subtitle>
-//                         )}
-//                         </a>
-                        
-//                         <br/>
-//                         {course.node.courseSeason}
-//                     </td>
-//                     <td className={styles.cell}><h6>{course.node.format}</h6></td>                                                                                                                                                                                
-//                     <td className={styles.cell}><h6>{course.node.topics}</h6></td>
-//                     <td className={styles.cell}><h6>{course.node.technologies}</h6></td>
-//                     <td className={styles.cell}><h6>{course.node.startDate} - {course.node.endDate}
-//                     <br/>{course.node.schedule}</h6></td>
-//                     <td className={styles.cell}><h6>{course.node.designedFor ? course.node.designedFor : ""}</h6></td>
-//                     <td className={styles.cell}><BrandButton className="p-1">Application Closed</BrandButton></td>
-//                 </tr>
-//             ))}
-            
-//         </Table>
-//     )
-// }
-
-
-
 
 const CourseTable = ({ tableColumns, tableInfo }) => {
 
