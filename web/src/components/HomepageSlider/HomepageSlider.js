19c74994b00e3e1d4c288c8071d0bcc950567c04
raney24 <raney24fcb@gmail.com>
2023-06-20 16:30:55 -0400

    initial changes or fixes to svg

--- web/src/components/HomepageSlider/HomepageSlider.js
+++ web/src/components/HomepageSlider/HomepageSlider.js
@@ -1,26 +1,15 @@
 import React from "react";
 import { graphql, Link, useStaticQuery } from 'gatsby'
 // JSX
-import HeroSlider, { Slide, Nav, OverlayContainer } from "hero-slider";
+import HeroSlider, { Slide, Nav, Overlay } from "hero-slider";
 import Wrapper from "../UI/Wrapper/Wrapper";
 import Title from "../UI/Title/Title";
 import Subtitle from "../UI/Subtitle/Subtitle";
-import BrandButton from "../UI/BrandButton/BrandButton"
+import BrandButton from "../UI/BrandButton/BrandButton";
 // import { ButtonContainer } from "./BasicSlider.module.scss"
 import { Container } from "react-bootstrap"
 
-// TODO: Convert these images to GatsbyImage and use Sanity to source them
-const workspace_hero_image = "../HomepageSlider/"
-const bootcamp_hero_image = "./hero-image-2-min.jpg"
-const fellowship_hero_image = "./hero-image-3-min.jpg"
-
-// Images
-// const bogliasco = "https://i.imgur.com/Gu5Cznz.jpg";
-const countyClare = "https://i.imgur.com/idjXzVQ.jpg";
-const craterRock = "https://i.imgur.com/8DYumaY.jpg";
-const giauPass = "https://i.imgur.com/8IuucQZ.jpg";
-
-const HomepageSlider = props => {
+const HomepageSlider = (props) => {
 
   const query = useStaticQuery(graphql`
     query imageSlider {
@@ -46,31 +35,48 @@ const HomepageSlider = props => {
   const slides = (query.sanityImageSlider.slides || {});
   return (
     <HeroSlider
-      slidingAnimation="left_to_right"
-      orientation="horizontal"
-      initialSlide={1}
+      // slidingAnimation="left_to_right"
+      // orientation="horizontal"
+      // initialSlide={1}
       // onBeforeChange={(previousSlide, nextSlide) =>
       //   console.log("onBeforeChange", previousSlide, nextSlide)
       // }
       // onChange={nextSlide => console.log("onChange", nextSlide)}
       // onAfterChange={nextSlide => console.log("onAfterChange", nextSlide)}
-      style={{
-        backgroundColor: "rgba(0, 0, 0, 0.33)"
-      }}
-      settings={{
-        slidingDuration: 250, 
+      // style={{
+      //   backgroundColor: "rgba(0, 0, 0, 0.33)"
+      // }}
+      // settings={{
+      //   slidingDuration: 250, 
+      //   slidingDelay: 100,
+      //   shouldAutoplay: true,
+      //   shouldDisplayButtons: true,
+      //   autoplayDuration: 5000,
+      //   height: "100vh"
+      // }}
+      controller={{
+        initialSlide: 1,
+        slidingDuration: 500,
         slidingDelay: 100,
-        shouldAutoplay: true,
-        shouldDisplayButtons: true,
-        autoplayDuration: 5000,
-        height: "100vh"
+        onSliding: (nextSlide) =>
+          console.debug("onSliding(nextSlide): ", nextSlide),
+        onBeforeSliding: (previousSlide, nextSlide) =>
+          console.debug(
+            "onBeforeSliding(previousSlide, nextSlide): ",
+            previousSlide,
+            nextSlide
+          ),
+        onAfterSliding: (nextSlide) =>
+          console.debug("onAfterSliding(nextSlide): ", nextSlide)
       }}
+      autoplay
+      height={"100vh"}
     >
       {slides.map((slide,i) => (
-        <OverlayContainer key={i}>
+        <Overlay key={i}>
           <Slide
             background={{
-              backgroundImage: slide.image.asset.url,
+              backgroundImageSrc: slide.image.asset.url,
               backgroundAttachment: "fixed"
             }}
             >
@@ -82,11 +88,10 @@ const HomepageSlider = props => {
               </Container>
             </Wrapper>
           </Slide>
-        </OverlayContainer>
+        </Overlay>
       ))}
       <Nav />
     </HeroSlider>
-
     
   );
 };
