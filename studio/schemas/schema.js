ddea09c09b297df12ea871e8e14f0d00c9dc7ee2
raney24 <raney24fcb@gmail.com>
2023-08-22 16:05:33 -0400

    update sanity [[incomplete]]

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,9 +1,3 @@
-// First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
-
-// Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-
 // Object types
 import bioPortableText from './objects/bioPortableText'
 import figure from './objects/figure'
@@ -49,14 +43,7 @@ import bootcampClass from './documents/bootcampClass'
 import notes from './documents/notes'
 
 // Then we give our schema to the builder and provide the result to Sanity
-export default createSchema({
-  // We name our schema
-  name: 'ainc',
-  // Then proceed to concatenate our our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
+export default [
     bioPortableText,
     figure,
     projectMember,
@@ -99,5 +86,4 @@ export default createSchema({
     pageTitles,
     notes,
     teamAlpha,
-  ])
-})
+ ]
