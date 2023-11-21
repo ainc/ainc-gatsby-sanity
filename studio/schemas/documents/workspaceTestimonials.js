6a1376ff3da70fb0651798fae8ac717cadda288b
metraidep <thtranle.2304@gmail.com>
2023-11-20 20:02:41 -0500

    remove yarn.lock and add workspaceTestimonials.js

---
+++ studio/schemas/documents/workspaceTestimonials.js
@@ -0,0 +1,18 @@
+export default {
+    name: 'workspaceTestimonials',
+    type: 'document',
+    title: 'Workspace Testimonials',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Full Name'
+      },
+      {
+        name: 'testimonial',
+        type: 'string',
+        title: 'Testimonial'
+      }
+    ]
+}
+   
