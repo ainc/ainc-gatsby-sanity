3666bb8c77fa91fc84860a2a919e1020ece36cd2
raney24 <raney24fcb@gmail.com>
2023-12-04 16:50:34 -0500

    fix bg card blank style

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -10,7 +10,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   flags: {
-    // DEV_SSR: true // enable server-side rendering to bypass `netlify build`
+    DEV_SSR: true // enable server-side rendering to bypass `netlify build`
   },
   plugins: [
     'gatsby-plugin-postcss',
--- web/src/components/BackgroundCard/BackgroundCard.js
+++ web/src/components/BackgroundCard/BackgroundCard.js
@@ -18,13 +18,13 @@ const BackgroundCard = (props) => {
   };
 
   return (
-    <a href onClick={() => scrollToSection(props.sectionRef)}>
+    <a onClick={() => scrollToSection(props.sectionRef)}>
       <Card className={`${styles.cardBackgroundFilter} col-xs-12`} style={{ backgroundImage: `url(${props.backgroundImage})` }} 
       onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
         <Card.Body className="card-body--background-card p-1">
-          <Card.Img variant="top" src={props.imgSrc} alt={props.alt} className="d-block mx-auto mt-3 w-50" style={{transform: isHovered ? 'scale(1.1)' : '', transition: '0.3s'}}/>
-          <Card.Title className={`text-uppercase text-white text-center fw-bold mt-3 ${styles.cardTitle}`} style={{transform: isHovered ? 'scale(1.1)' : '', transition: '0.3s'}}>{props.title}</Card.Title>
-          <Card.Text className={`text-white text-center mb-5 ${styles.cardText}`} style={{transform: isHovered ? 'scale(1.1)' : '', transition: '0.3s'}}>{props.text}</Card.Text>
+          <Card.Img variant="top" src={props.imgSrc} alt={props.alt} className="d-block mx-auto mt-3 w-50" style={{transform: isHovered ? 'scale(1.1)' : 'scale(1.0)', transition: '0.3s'}}/>
+          <Card.Title className={`text-uppercase text-white text-center fw-bold mt-3 ${styles.cardTitle}`} style={{transform: isHovered ? 'scale(1.1)' : 'scale(1.0)', transition: '0.3s'}}>{props.title}</Card.Title>
+          <Card.Text className={`text-white text-center mb-5 ${styles.cardText}`} style={{transform: isHovered ? 'scale(1.1)' : 'scale(1.0)', transition: '0.3s'}}>{props.text}</Card.Text>
         </Card.Body>
       </Card>
     </a>
