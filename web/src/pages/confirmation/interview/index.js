2fedc295f4ca439cb2e769db6878ea1a0e480a4a
metraidep <thtranle.2304@gmail.com>
2024-01-02 11:24:56 -0500

    add link hover and custom hook useSearchParams

--- web/src/pages/confirmation/interview/index.js
+++ web/src/pages/confirmation/interview/index.js
@@ -4,9 +4,20 @@ import Layout from '../../../components/Layout/Layout';
 import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import * as styles from "../interview/interview.module.scss"
 import { FaSquare } from 'react-icons/fa';
+import {
+    Routes,
+    Route,
+    useSearchParams,
+    BrowserRouter
+  } from "react-router-dom"
+
+  
 
 const InterviewPage = () => {
     const [isTimerComplete, setIsTimerComplete] = useState(false);
+    const [queryParameters] = useSearchParams()
+
+   
 
     const handleTimerComplete = () => {
       setIsTimerComplete(true);
@@ -20,13 +31,13 @@ const InterviewPage = () => {
                     <h1 className=" my-1">Bootcamp Interview Schedule</h1>
                 </Row>
                 <Row>
-                    <p  className='ms-3' style={{fontSize: "1.5 rem"}}>  Thank you for scheduling a call with us. Please check your inbox and confirm that you can make the scheduled time. </p>
+                    <p  className='ms-3' style={{fontSize: "1.5 rem"}}>  Thank you for scheduling a call with us {queryParameters.get("assigned_to")}. Please check your inbox and confirm that you can make the scheduled time. </p>
                 </Row>
                 <Row className='justify-content-center'> 
                     {!isTimerComplete ? (
                         <CountdownCircleTimer 
                             isPlaying
-                            duration={290} // 4 minutes 50 seconds
+                            duration={290} // 4 minutes 50 seconds 
                             onComplete={handleTimerComplete}
                             colors={[['#ff0000']]}
                             strokeLinecap = {'square'}
@@ -49,7 +60,7 @@ const InterviewPage = () => {
                     <ul className='ms-5'>
                         <li>
                             <p>
-                                <a style={{fontSize: "1.5 rem"}} href="https://forms.zohopublic.com/virtualoffice9155/form/BootcampApplicationBasicChallengesF20/formperma/eyRW9Evft9ta2yt2rE4dpWtevNxIo_W804G-905H9d4">
+                                <a  className = {styles.link} style={{fontSize: "1.5 rem"}} href="https://forms.zohopublic.com/virtualoffice9155/form/BootcampApplicationBasicChallengesF20/formperma/eyRW9Evft9ta2yt2rE4dpWtevNxIo_W804G-905H9d4">
                                     Before your scheduled Call, please complete these basic challenges.
                                 </a>
                             </p>
@@ -57,7 +68,7 @@ const InterviewPage = () => {
                         </li>
                         <li>
                             <p>
-                            <a style={{fontSize: "1.5 rem"}} href="https://www.awesomeinc.org/blog/what-we-look-for-bootcamp-student">
+                            <a  className = {styles.link} style={{fontSize: "1.5 rem"}} href="https://www.awesomeinc.org/blog/what-we-look-for-bootcamp-student">
                                Want to know what we'll talk about on our call? Check out our blog here!
                             </a>
                             </p>
