e12801bf1b0856e15eab50ed0a934521567cc456
ColinCarver10 <ccarver410@gmail.com>
2024-03-18 11:10:48 -0400

    Refactor to pull from Sanity

--- web/.sanity/runtime/app.js
+++ web/.sanity/runtime/app.js
@@ -2,7 +2,8 @@
 // This file is auto-generated on 'sanity dev'
 // Modifications to this file is automatically discarded
 import {renderStudio} from "sanity"
-import studioConfig from "../../sanity.config.js"
+
+const studioConfig = {missingConfigFile: true}
 
 renderStudio(
   document.getElementById("sanity"),
---
+++ web/src/components/WhatWeDo/WhatWeDo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+import { Container, Image, Row} from "react-bootstrap";
+import * as styles from "./WhatWeDo.module.scss";
+import Subtitle from '../UI/Subtitle/Subtitle'
+import redAsterisk from "../../images/what-we-do/red-asterisk.png";
+
+const WhatWeDoBox = ( {item} ) => {
+    const partnerships = ['Startup Lexington', 'Startup Weekend', 'UndrCvr Lex'];
+    const description = item.description.replace(/\s([^\s<]+)\s*$/,'\u00A0$1'); // Remove orphan words.
+    return (
+    <Container className="d-flex flex-column justify-content-center align-items-center text-center">
+        <div className={`${styles.logoContainer} d-flex flex-column justify-content-center align-items-center text-center`}>
+            <a href={item.linkToPage} className="d-flex align-items-center">
+                <Image className={`${styles.logo}`} src={item.logo.asset.url} alt={item.title}/>
+                {partnerships.includes(item.title) && <Image className={`${styles.asterisk}`} src={redAsterisk} alt="Red asterisk"/>}
+            </a>
+        </div>
+        <p className={`${styles.logoDescription}  mt-auto`}>{description}</p>
+    </Container>
+    )
+}
+
+export default WhatWeDoBox
