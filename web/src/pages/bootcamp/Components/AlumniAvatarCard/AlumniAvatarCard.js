8bd37316a0052578cdf39be2baad4086d9ea4656
Ethan Gamble <ethangamble37@outlook.com>
2024-04-18 16:54:44 -0400

    bootcamp cards

--- web/src/pages/bootcamp/Components/AlumniAvatarCard/AlumniAvatarCard.js
+++ web/src/pages/bootcamp/Components/AlumniAvatarCard/AlumniAvatarCard.js
@@ -1,11 +1,12 @@
 import  React from 'react';
-import { Col } from "react-bootstrap";
+import { Col, Card } from "react-bootstrap";
 import { GatsbyImage } from "gatsby-plugin-image";
 import * as styles from './AlumniAvatarCard.module.scss'
 
 
 export default function AlumniAvatarCard ({ alumniImage, name, jobTitle, companyLogo }) {
     return (
+    <Card className='' style={{width: 'fit-content', backgroundColor: '#d1d3d4'}}>
         <Col className="flex-column justify-content-center align-items-center">
             <GatsbyImage className={styles.alumniImage} image={alumniImage} alt={name}/>
             <h3 className={styles.name}>{name}</h3>
@@ -14,5 +15,6 @@ export default function AlumniAvatarCard ({ alumniImage, name, jobTitle, company
                 <GatsbyImage className={styles.companyLogo} image={companyLogo} alt={jobTitle}/>
             </div>
         </Col>
+    </Card>
     )   
 }
--- web/src/pages/bootcamp/Components/AlumniAvatarCard/AlumniAvatarCardCarousel.js
+++ web/src/pages/bootcamp/Components/AlumniAvatarCard/AlumniAvatarCardCarousel.js
@@ -1,89 +1,56 @@
 import React, { useEffect, useState, useRef } from "react";
-import { motion, useMotionValue } from "framer-motion";
 import AlumniAvatarCard from "./AlumniAvatarCard";
-import { Row } from "react-bootstrap";
-
-const ONE_SECOND = 1000;
-const AUTO_DELAY = ONE_SECOND * 10;
-const DRAG_BUFFER = 50;
-
-const SPRING_OPTIONS = {
-  type: "spring",
-  mass: 3,
-  stiffness: 400,
-  damping: 50,
-};
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export const SwipeCarousel = ({ featuredAlumni }) => {
-  const [imgIndex, setImgIndex] = useState(0);
 
-  const dragX = useMotionValue(0);
-
-  /*
-  useEffect(() => {
-    const intervalRef = setInterval(() => {
-      const x = dragX.get();
-
-      if (x === 0) {
-        setImgIndex((pv) => {
-          if (pv === featuredAlumni.length - 1) {
-            return 0;
-          }
-          return pv + 1;
-        });
+  const settings = {
+    className: "center",
+    dots: false,
+    infinite: true,
+    swipeToSlide: true,
+    slidesToShow: 5,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
       }
-    }, AUTO_DELAY);
-
-    return () => clearInterval(intervalRef);
-  }, []);
-
-  const onDragEnd = () => {
-    const x = dragX.get();
-
-    if (x <= -DRAG_BUFFER && imgIndex < featuredAlumni.length - 1) {
-      setImgIndex((pv) => pv + 1);
-    } else if (x >= DRAG_BUFFER && imgIndex > 0) {
-      setImgIndex((pv) => pv - 1);
-    }
+    ]
   };
-  */
-  const ref = useRef(null);
 
   return (
-    <div className="relative overflow-hidden py-8" style={{backgroundColor: '#e6e7e8'}} ref={ref}> 
-      <motion.div
-        drag="x"
-        dragConstraints={ref}
-        style={{
-          x: dragX,
-        }}
-        animate={{
-          translateX: `-${imgIndex * 20}%`,
-        }}
-        transition={SPRING_OPTIONS}
-        className="flex cursor-grab items-center active:cursor-grabbing"
-      >
-        <Images featuredAlumni={featuredAlumni} imgIndex={imgIndex}/>
-      </motion.div>
+    <div className="relative overflow-hidden slider-container" style={{ backgroundColor: '#e6e7e8' }}>
+      <Slider {...settings}>
+          {featuredAlumni.map((alumni, idx) => (
+          <div key={idx} className='p-3' style={{ display: 'inline-block' }}>
+            <AlumniAvatarCard
+                key={idx}
+                alumniImage={alumni.picture.asset.gatsbyImageData}
+                name={alumni.name}
+                jobTitle={alumni.jobTitle}
+                companyLogo={alumni.companyLogo.asset.gatsbyImageData}
+              />
+          </div>
+          ))}
+      </Slider>
     </div>
   );
 };
 
-const Images = ({ featuredAlumni, imgIndex }) => {
-  return (
-    <Row className="p-3" style={{flexWrap: 'nowrap'}}>
-      {featuredAlumni.map((alumni, idx) => {
-        return (
-            <AlumniAvatarCard alumniImage={alumni.picture.asset.gatsbyImageData} 
-                              name={alumni.name} 
-                              jobTitle={alumni.jobTitle} 
-                              companyLogo={alumni.companyLogo.asset.gatsbyImageData}
-                              />
-        );
-      })}
-    </Row>
-  );
-};
 
 
 
