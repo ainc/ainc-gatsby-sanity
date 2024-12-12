ce28fc01db0cfbb4039a5ce8e1095fc57e4604d3
nwils000 <nwils000@gmail.com>
2024-12-12 11:42:44 -0500

    banner padding shows conditionally as well as no padding around the layout

--- web/src/components/Banner/Banner.js
+++ web/src/components/Banner/Banner.js
@@ -1,16 +1,15 @@
-import { graphql, useStaticQuery } from 'gatsby'
-import React, { useState, useEffect } from 'react';
-import * as styles from './Banner.module.scss'
+import { graphql, useStaticQuery } from "gatsby";
+import React, { useState, useEffect } from "react";
+import * as styles from "./Banner.module.scss";
 import { useLocation } from "@reach/router";
 
 const Banner = () => {
-
   const bannerData = useStaticQuery(
     graphql`
       query newfiveAcrossQuery($currentDate: Date) {
         allSanityEvents(
-          filter: {reference: {eventTypeName: {eq: "5 Across"}}, date: {gte: $currentDate}}
-          sort: {date: ASC}
+          filter: { reference: { eventTypeName: { eq: "5 Across" } }, date: { gte: $currentDate } }
+          sort: { date: ASC }
         ) {
           nodes {
             eventName
@@ -19,30 +18,38 @@ const Banner = () => {
             location
             linkToEvent
             picture {
-            asset {
+              asset {
                 gatsbyImageData
-                }
+              }
             }
             reference {
-            eventTypeName
+              eventTypeName
             }
           }
         }
-    }`
-  )
+      }
+    `
+  );
 
   const currentDate = new Date().toISOString().slice(0, 10);
 
-  const upcoming = bannerData.allSanityEvents.nodes.filter(event => new Date(event.date) >= new Date(currentDate));
-  const showUpcoming = currentDate > upcoming ? null : <p>Sign up <a href={upcoming[0].linkToEvent}>here</a> for 5 Across - {upcoming[0].date} </p>; 
+  const upcoming = bannerData.allSanityEvents.nodes.filter(
+    event => new Date(event.date) >= new Date(currentDate)
+  );
+  const showUpcoming =
+    currentDate > upcoming ? null : (
+      <p>
+        Sign up <a href={upcoming[0].linkToEvent}>here</a> for 5 Across - {upcoming[0].date}{" "}
+      </p>
+    );
 
   return (
-    useLocation().pathname !== '/bootcamp/' && (
-      <div role="banner" className={`${styles.banner}`}>
-      { showUpcoming }
-    </div>
+    useLocation().pathname !== "/bootcamp/" && (
+      <div role="banner" className={`${styles.banner} ${!showUpcoming ? styles.noPadding : ""}`}>
+        {showUpcoming}
+      </div>
     )
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
