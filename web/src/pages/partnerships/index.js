bc1a127fcba799f2e4b565946ecc3b41e555449d
jrholt04 <jrholt04@gmail.com>
2025-02-07 15:01:20 -0500

    I added the partnhership page and styles

---
+++ web/src/pages/partnerships/index.js
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import SEO from "../../components/seo";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../../components/Layout/Layout.js";
+import BackgroundCard from "../../components/BackgroundCard/BackgroundCard";
+import { motion } from "framer-motion";
+import Title from "../../components/UI/Title/Title";
+import * as styles from "./partnership.module.scss";
+
+
+
+
+const PartnershipsPage = ({ data }) => {
+    return (
+        <Layout>
+            <Container fluid className={`${styles.mainHeading}`}>
+                <Row>
+                    <Col className="offset-sm-2" style={{ marginTop: "30%" }}>
+                        <motion.div
+                            initial={{ opacity: 0, y: -50 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ delay: 0.5, duration: 0.8 }}
+                        >
+                        <Row className="">
+                        </Row>
+                        <Row>
+                            <Col md="8" className="">
+                            <Title className={`text-white mb-5`}>
+                                Meet Our Awesome Partners 
+                                <p>Dedicated to curating Lexington's finest startups</p>
+                            </Title>
+                            </Col>
+                        </Row>
+                        </motion.div>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{ delay: 1.0, duration: 0.4 }}
+                        >
+                        </motion.div>
+                    </Col>
+                </Row>
+            </Container>
+        </Layout>
+    );
+};
+
+export default PartnershipsPage;
