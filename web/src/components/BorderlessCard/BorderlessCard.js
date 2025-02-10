2746bf577e99050ef5b73c113e040361fdcffe7b
Ricky Chen <ricky.chenwok@gmail.com>
2025-02-10 11:59:20 -0500

    added google maps embeded code + modal pop up

--- web/src/components/BorderlessCard/BorderlessCard.js
+++ web/src/components/BorderlessCard/BorderlessCard.js
@@ -9,10 +9,13 @@ import * as styles from "./BorderlessCard.module.css";
 const MemberOption = (props) => {
   const [showModal1, setShowModal1] = useState(false);
   const [showModal2, setShowModal2] = useState(false);
+  const [showModal3, setShowModal3] = useState(false);
   const handleCloseModal1 = () => setShowModal1(false);
   const handleShowModal1 = () => setShowModal1(true);
   const handleCloseModal2 = () => setShowModal2(false);
   const handleShowModal2 = () => setShowModal2(true);
+  const handleCloseModal3 = () => setShowModal3(false);
+  const handleShowModal3 = () => setShowModal3(true);
 
   return (
     <div>
@@ -88,6 +91,30 @@ const MemberOption = (props) => {
               </div>
             </Row>
           </Container>
+        }
+      />
+
+      {/* adding parking space embeded code, need to link to parking map button */}
+      <ModalCustom
+        lgShow={showModal3}
+        hide={handleCloseModal3}
+        content={
+          <Row>
+            <div>
+              <iframe
+                src="https://www.google.com/maps/d/u/0/embed?mid=1tuKBp8oRRCexyub92RobT_smUMI"
+                width="100%"
+                height="400"
+              ></iframe>
+              <p></p>
+              <a
+                href="https://www.google.com/maps/d/u/0/viewer?ll=38.04159812410385%2C-84.49238033250617&hl=en&f=q&hq=parking%20near%20awesome%20inc&source=s_q&spn=0.003333%2C0.004823&t=h&geocode&msa=0&z=18&ie=UTF8&hnear&mid=1tuKBp8oRRCexyub92RobT_smUMI"
+                target="_blank"
+              >
+                Click Here to View in Google
+              </a>
+            </div>
+          </Row>
         }
       />
     </div>
