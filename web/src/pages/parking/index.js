2bf9c0259277debe1884aa1bbd2db307635e1cad
Ricky Chen <ricky.chenwok@gmail.com>
2025-02-10 11:59:20 -0500

    added google maps embeded code + modal pop up

--- web/src/pages/parking/index.js
+++ web/src/pages/parking/index.js
@@ -6,10 +6,22 @@ import Layout from "../../components/Layout/Layout";
 import Title from "../../components/UI/Title/Title";
 import Subtitle from "../../components/UI/Subtitle/Subtitle";
 import BrandButton from "../../components/UI/BrandButton/BrandButton";
-import * as styles from "./parking.module.css";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import * as styles from "../../components/BorderlessCard/BorderlessCard.module.css";
+import { useState } from "react";
+import ModalCustom from "../../components/Modal/ModalCustom";
 
 const ParkingPage = ({ data }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <Layout>
       <Container className="ps-3">
@@ -29,13 +41,14 @@ const ParkingPage = ({ data }) => {
           </p>
         </Col>
         <Row>
+          {/* Parking map Button  with alignment*/}
           <Col
             xs={{ offset: 2, span: 8 }}
             md={{ offset: 4, span: 4 }}
             lg={{ offset: 4 }}
             xl={{ offset: 4, span: 5 }}
           >
-            <a
+            {/* <a
               target="_blank"
               href="https://www.google.com/maps/d/u/0/viewer?ll=38.041576999999975%2C-84.49243799999999&hl=en&f=q&hq=parking%20near%20awesome%20inc&source=s_q&spn=0.003333%2C0.004823&t=h&geocode&msa=0&z=18&ie=UTF8&hnear&mid=1tuKBp8oRRCexyub92RobT_smUMI"
             >
@@ -43,7 +56,18 @@ const ParkingPage = ({ data }) => {
                 Parking map
                 <FaMapMarkerAlt className="mb-1 ms-2"></FaMapMarkerAlt>
               </BrandButton>
-            </a>
+            </a>  */}
+
+            {/* aerial view is complete, in RC's google docs */}
+            <div>
+              <BrandButton
+                className={`mb-4 mt-5 ${styles.button} `}
+                onClick={openModal}
+              >
+                Parking Map
+                <FaMapMarkerAlt className="mb-1 ms-2"></FaMapMarkerAlt>
+              </BrandButton>
+            </div>
           </Col>
         </Row>
         <Col
@@ -113,6 +137,50 @@ const ParkingPage = ({ data }) => {
           </ul>
         </Col>
       </Container>
+
+      {/* Modal for Parking Map */}
+      <ModalCustom
+        lgShow={isModalOpen}
+        hide={closeModal}
+        title="Parking Map"
+        content={
+          <Container>
+            <Row>
+              <Col className="text-center">
+                <Subtitle>Here’s the parking map for Awesome Inc</Subtitle>
+              </Col>
+            </Row>
+            <Row>
+              {/* Embedded Google Map */}
+              <iframe
+                src="https://www.google.com/maps/d/u/0/embed?mid=1tuKBp8oRRCexyub92RobT_smUMI"
+                width="100%"
+                height="400"
+                style={{ border: 0 }}
+                allowFullScreen
+              ></iframe>
+            </Row>
+            <Row className="mt-3">
+              <Col className="d-flex justify-content-center">
+                {/* Link to the full Google Maps page */}
+                <a
+                  href="https://www.google.com/maps/d/u/0/viewer?mid=1tuKBp8oRRCexyub92RobT_smUMI"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-decoration-none"
+                >
+                  <BrandButton>
+                    Open Full Parking Map in Google Maps
+                  </BrandButton>
+                </a>
+              </Col>
+            </Row>
+            <Row className="mt-3">
+              <BrandButton onClick={closeModal}>Close</BrandButton>
+            </Row>
+          </Container>
+        }
+      />
     </Layout>
   );
 };
