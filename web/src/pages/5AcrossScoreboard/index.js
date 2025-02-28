a9d801e505474985d72a8992df2a4d74c1ddfe4f
jrholt04 <jrholt04@gmail.com>
2025-02-28 16:34:24 -0500

    fixed the rank feature

--- web/src/pages/5AcrossScoreboard/index.js
+++ web/src/pages/5AcrossScoreboard/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import { graphql } from "gatsby";
-
+import * as styles from "./5AcrossScoreboard.module.scss";
 import Layout from "../../components/Layout/Layout";
 import { Container, Row, Col } from "react-bootstrap";
 import Title from "../../components/UI/Title/Title";
@@ -9,26 +9,72 @@ import Title from "../../components/UI/Title/Title";
 const FiveAcrossScoreboard = ({ data }) => {
     // Extract the nodes array from the data structure and sort them in order by points 
     const nodes = data.allGoogleSheet.nodes[0].Sheet1;
-    nodes.sort((a, b) => b.points - a.points);
+    const copyNodes = data.allGoogleSheet.nodes[0].Sheet1;
+    nodes.sort((a, b) => b.points - a.points);   
+    nodes.forEach((node, index) => {
+        node.rank = index + 1; 
+    });
+
+    // State to manage filtered data
+    const [filteredData, setFilteredData] = useState(nodes);
+
     
-    
+    const filterByName = (data, name) => {
+        const nodes = data.allGoogleSheet.nodes[0].Sheet1;
+       
+        if (!name) {
+            return copyNodes; 
+        }
+
+        const filteredData = nodes.filter((item) =>
+          item.fName.toLowerCase().includes(name.toLowerCase())
+        );
+        return filteredData;
+      };
+
+      // Handle form submission
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+       
+        const formData = new FormData(e.target);
+        
+        const name = formData.get("name");
+
+        const filteredData = filterByName(data, name);
+        setFilteredData(filteredData);
+    };
+
+
     return(
    <Layout>
         <Col className="p-5">
         <Row>
             <Title
                 style={{ color: "#ED3742" }}
-                className="text--huge text-center mt-4 "
+                className="text--huge text-center mt-4  mx-center"
             >
-                5 across scoreboard
-                <p>
+                Attendes standings
+                <p style={{ color: 'black', width: '75%'}} className="mx-auto">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </Title>
         </Row>
-        <table className="table" border={3}>
+
+        <form  className="p-1" id="filterForm" onSubmit={handleSubmit}>
+        <label className="px-2" htmlFor="name">Enter Name:</label>
+            <input
+                className="px-2"
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Enter name"
+            />
+            <button className="mx-2" type="submit">Filter</button>
+        </form>
+
+        <table className={`${styles.table}`}>
             <thead>
-                <tr>
+                <tr className="text-white">
                 <th scope="col">Rank</th>
                 <th scope="col">Name</th>
                 <th scope="col">Events attended</th>
@@ -36,9 +82,9 @@ const FiveAcrossScoreboard = ({ data }) => {
                 </tr>
             </thead>
             <tbody>
-                {nodes.map((item, index) => (
+                {filteredData.map((item, index) => (
                     <tr>
-                    <th scope="row">{index + 1}</th>
+                    <th scope="row"  className="text-white">{item.rank}</th>
                     <td>{item.fName} {item.lName.charAt(0)}.</td>
                     <td>{item.totalEvents}</td>
                     <td>{item.points}</td>
@@ -48,6 +94,7 @@ const FiveAcrossScoreboard = ({ data }) => {
         </table>          
         </Col>
     </Layout>
+  
    )
 }
 export const query_scoreboard = graphql`
