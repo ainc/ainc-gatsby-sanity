de55a2e3b7ada29ce678b4a25d7eba4c058393c3
jrholt04 <jrholt04@gmail.com>
2025-03-03 16:24:55 -0500

    got rid of email hard coded

--- web/generate-credentials.js
+++ web/generate-credentials.js
@@ -9,7 +9,9 @@ const template = fs.readFileSync("test-scoreboard-template.json", "utf-8");
 // Replace placeholders with actual environment variables
 const filledJson = template
   .replace("ENV_PRIVATE_KEY_ID", process.env.FIVEACROSS_SCOREBOARD_KEY_ID)
-  .replace("ENV_PRIVATE_KEY", process.env.FIVEACROSS_SCOREBOARD_KEY.replace(/\\n/g, "\n"));
+  .replace("ENV_PRIVATE_KEY", process.env.FIVEACROSS_SCOREBOARD_KEY.replace(/\\n/g, "\n"))
+  .replace("GOOGLE_CLOUD_EMAIL,", Process.env.GOOGLE_CLOUD_EMAIL);
+
 
 // Write the new JSON file
 fs.writeFileSync("test-scoreboard-credentials.json", filledJson);
