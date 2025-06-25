d38294c741f3f0b91116519f9c6fed1d9a0c0417
wernickinick <wernickin@gmail.com>
2025-06-25 13:42:01 -0400

    add assets for parking

--- web/src/pages/parking/index.js
+++ web/src/pages/parking/index.js
@@ -1,17 +1,40 @@
 import * as React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Image } from "react-bootstrap";
 import SEO from "../../components/seo";
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
 import Layout from "../../components/Layout/Layout";
 import Title from "../../components/UI/Title/Title";
 import Subtitle from "../../components/UI/Subtitle/Subtitle";
 import * as styles from "./parking.module.css";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import ParkingMap from "../../images/parkingraphicversion2.jpg"
+import ParkingIcon from "../../images/parkingicon.png";
 
 const ParkingPage = ({ data }) => {
   return (
     <Layout>
       <div className={styles.parking}></div>
+      
+      <Col
+          xs={{ span: 20 }}
+          lg={{ offset: 5, span: 10 }}
+          xl={{ offset: 2, span: 8 }}
+        >
+         
+          <Image
+            placeholder="blurred"
+            className="position-absolute"
+            style={{top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 1,
+            width: '80%',
+            height: 'auto',}}
+            src={ParkingIcon}
+            alt="awesome inc parking lot"
+          />
+          </Col>
+      
       <Container className="ps-3">
         <Col
           xs={{ offset: 1, span: 10 }}
@@ -43,12 +66,13 @@ const ParkingPage = ({ data }) => {
           lg={{ offset: 5, span: 10 }}
           xl={{ offset: 1, span: 10 }}
         >
-          <StaticImage
+          <Image
             placeholder="blurred"
             className="mb-4"
-            src="/Users/nicholas/Desktop/parkingraphicversion2.jpg"
+            style={{ width: '100%', height: 'auto'}}
+            src={ParkingMap}
             alt="awesome inc parking lot"
-          ></StaticImage>
+          />
         </Col>
       </Container>
       
@@ -66,7 +90,6 @@ const ParkingPage = ({ data }) => {
           </Row>
           <ul className={`${styles.Check} ${styles.stableList}`}>
             <li>Spaces marked for Awesome Inc in the lot East of our building</li>
-            <li>Free 2-hour parking spaces along East Vine Street</li>
             <li>Free full-day spaces on Short Street near Thoroughbred Park and Ransom Ave near Second Presbyterian church</li>
             <li>Deweese Street (PNC Bank) Parking Garage, ~$4/day</li>
             <li>Metered spaces on Water Street, Main Street, or Eastern Avenue</li>
@@ -84,8 +107,8 @@ const ParkingPage = ({ data }) => {
             </Col>
           </Row>
           <ul className={`${styles.Xmark} ${styles.stableList}`}>
-            <li>Spaces marked for Carson's Restaurant or PPG Paints (East Side of building)</li>
-            <li>Spaces marked for Commerce Lexington, Northwestern Mutual, Tate/Hill/Jacobs Architects, or Awesome Inc Directors (West side of building)</li>
+            <li>Spaces marked for Carson's Restaurant (East Side of building)</li>
+            <li>Spaces marked for Commerce Lexington, Northwestern Mutual or Awesome Inc Directors (West side of building)</li>
             <li>Spaces in the Main and Vine retail / residential complex</li>
             <li>In the "Loading Zone" on Main Street, directly in front of Awesome Inc</li>
           </ul>
