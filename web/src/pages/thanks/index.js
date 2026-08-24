75129dadab4c687c96a34d937c8a0118fcc1b0f9
Tayinator <93267372+Tayinator@users.noreply.github.com>
2026-08-24 12:56:22 -0400

    404 and Thanks cards point at the correct pages
    Replaced thanks page with 404 code to start to add the needed image backgrounds back to the cards.
    
    Changed links to /fellowship and https://apaxsoftware.com/ for startup and software dev cards.
    
    Backgrounds now use the same local files as the homepage (/images/student-learn-to-code-shirt.jpg, awesome-inc-space-banner.jpg, startup-panel-min.jpg, software-panel.jpg) instead of the live-site URLs that 404.
    
    Each card uses its own icon (brackets, desk, rocket, laptop). The laptop icon is web/src/images/software-development.png so it is not the learn-to-code graphic.

--- web/src/pages/thanks/index.js
+++ web/src/pages/thanks/index.js
@@ -5,53 +5,65 @@ import SEO from "../../components/seo";
 import Title from "../../components/UI/Title/Title";
 import BackgroundCard from "../../components/BackgroundCard/BackgroundCard";
 import { Container, Col, Row } from "react-bootstrap";
+import LearnToCodeIcon from "../../images/Learn_To_Code.png";
+import RentWorkspaceIcon from "../../images/Rent_Workspace.png";
+import AccelerateStartupIcon from "../../images/accelerate-your-startup.png";
+import SoftwareDevelopmentIcon from "../../images/software-development.png";
 
 const ThanksPage = () => (
   <Layout>
     <Container>
-      <Title className="text-center my-5 text-uppercase">Thank you</Title>
+      <Title className="text-center my-5 text-uppercase">
+        Need some direction?
+      </Title>
       <Row className="mb-5 d-flex justify-content-center">
         <Row
-          className={` row row-cols-5 justify-content-center my-3 px-1 gx-2`}
+          className={` row row-cols-5 justify-content-center my-3 px-0 gx-2`}
         >
           <Col xs={6} sm={6} md={6} lg={3} xl={3} className="mt-3 ">
             <BackgroundCard
+              alt="brackets"
               title="Learn to Code"
               text="Everybody can and should learn to code, start today."
-              backgroundImage="https://www.awesomeinc.org/images/student-learning-to-code-panel.jpg"
-              imgSrc="https://d33wubrfki0l68.cloudfront.net/592e71aaecbd967bf40d6346937d2a5a78f502f7/bb4b9/images/icons/learn-to-code.png"
-              link="/learn"
+              backgroundImage="/images/student-learn-to-code-shirt.jpg"
+              imgSrc={LearnToCodeIcon}
+              link="/learn/"
             />
           </Col>
           <Col xs={6} sm={6} md={6} lg={3} xl={3} className="mt-3 ">
             <BackgroundCard
+              alt="desk and chair"
               title="Rent Workspace"
               text="Rent a desk or space for events, meetings, and more."
-              backgroundImage="https://www.awesomeinc.org/images/awesome-inc-space-panel.jpg"
-              imgSrc="https://d33wubrfki0l68.cloudfront.net/13acb6f3560e894a9e0eecc194c96f778fba858f/f6fb2/images/icons/rent-workspace.png"
-              link="/workspace"
+              backgroundImage="/images/awesome-inc-space-banner.jpg"
+              imgSrc={RentWorkspaceIcon}
+              link="/workspace/"
             />
           </Col>
           <Col xs={6} sm={6} md={6} lg={3} xl={3} className="mt-3 ">
             <BackgroundCard
+              alt="brackets"
               title="Accelerate your startup"
               text="We will help grow your business with a mentor-driven, accelerator program."
-              backgroundImage="https://www.awesomeinc.org/images/student-learning-to-code-panel.jpg"
-              imgSrc="https://d33wubrfki0l68.cloudfront.net/592e71aaecbd967bf40d6346937d2a5a78f502f7/bb4b9/images/icons/learn-to-code.png"
-              link="/learn"
+              backgroundImage="/images/startup-panel-min.jpg"
+              imgSrc={AccelerateStartupIcon}
+              link="/fellowship/"
             />
           </Col>
           <Col xs={6} sm={6} md={6} lg={3} xl={3} className="mt-3 ">
             <BackgroundCard
+              alt="brackets"
               title="Software Development"
               text="Let us create custom software for your business."
-              backgroundImage="https://www.awesomeinc.org/images/student-learning-to-code-panel.jpg"
-              imgSrc="https://d33wubrfki0l68.cloudfront.net/592e71aaecbd967bf40d6346937d2a5a78f502f7/bb4b9/images/icons/learn-to-code.png"
-              link="/learn"
+              backgroundImage="/images/software-panel.jpg"
+              imgSrc={SoftwareDevelopmentIcon}
+              link="https://apaxsoftware.com/"
             />
           </Col>
         </Row>
       </Row>
+      {/* <h1>NOT FOUND</h1> */}
+      {/* <p>You just hit a route that doesn&#39;t exist... the sadness.</p> */}
     </Container>
   </Layout>
 );
