bc1d1f974226a6790ca745953777d1a57e0f3ce1
oliviabarker <olivia.barker@centre.edu>
2026-09-03 11:09:46 -0400

    Fixed desktop and mobile discrepancies in Learn to Code dropdown

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -113,8 +113,11 @@ const Header = () => {
                 Learn To Code <AiFillCaretDown size={10} />
               </Nav.Link>
               <div className="hover-options">
-                <a href="/intro-to-web-development">Adult Intro Course</a>
+                <a href="/learn/youth/code">Youth Courses</a>
+                <a href="/learn/adults">Adult Courses</a>
                 <a href="/weekofcode">Kids Camps</a>
+                <a href="/salesforce">Salesforce Career Accelerator</a>
+                <a href="/intro-to-web-development">Adult Intro Course</a>
               </div>
             </Col>
 
@@ -165,6 +168,7 @@ const Header = () => {
                           <a href="/salesforce">
                             Salesforce Career Accelerator
                           </a>
+                          <a href="/intro-to-web-development">Adult Intro Course</a>
                         </Col>
                       </Row>
                     </Nav>
